533b412f9642a1d721506243b681a6d6230390bc
RoushAH <Roush.Adam.H@gmail.com>
2023-12-10 20:25:06 +0000

    Day 10 done

--- AoC10.py
+++ AoC10.py
@@ -1,48 +1,67 @@
-import time
-DEBUG = True
+DEBUG = False
 PIPES = {
     "F": ("D", "R"),
     "L": ("R", "U"),
     "J": ("L", "U"),
     "7": ("D", "L"),
     "-": ("L", "R"),
-    "|": ("D", "U")
+    "|": ("D", "U"),
 }
 EMPTY = "."
-OUTSIDE = "O"
 data_list = []
 active_paths = []
 path_length = 0
+visited = []
+winners = 0
 
 
-def pprint(list):
+def junk_pipe_scan(array):
+    print(visited)
+    answer = 0
+    for y, row in enumerate(array):
+        crossings = 0
+        crosser = EMPTY
+        for x, char in enumerate(row):
+            if char == "|" and (y, x) in visited:
+                crossings += 1
+            elif char == "-" and (y, x) in visited:
+                pass
+            elif char == "F" and (y, x) in visited:
+                crosser = "F"
+            elif char == "7" and (y, x) in visited:
+                if crosser == "L":
+                    crossings += 1
+                    crosser = EMPTY
+            elif char == "L" and (y, x) in visited:
+                crosser = "L"
+            elif char == "J" and (y, x) in visited:
+                if crosser == "F":
+                    crossings += 1
+                    crosser = EMPTY
+            else:
+                answer += crossings % 2
+                array[y][x] = crossings
+                # print((y, x),char)
+                # check to see how many times the moat's been crossed
+    return array, answer
+
+
+def pprint(list, split=1):
     print()
-    for row in list:
-        disp = ""
-        for char in row:
-            disp += char
-        print(disp)
-
-
-def fill_outer_cells(x, y):
-    data_list[y][x] = OUTSIDE
-    if y > 0 and data_list[y-1][x] == EMPTY:
-        fill_outer_cells(x, y-1)
-    if x < len(data_list[0]) - 1 and data_list[y][x+1] == EMPTY:
-        fill_outer_cells(x+1, y)
-    if y < len(data_list) - 1 and data_list[y+1][x] == EMPTY:
-        fill_outer_cells(x, y+1)
-    if x > 0 and data_list[y][x-1] == EMPTY:
-        fill_outer_cells(x-1, y)
-
-
-def count_empties():
-    empties = 0
-    for row in data_list:
-        for char in row:
-            if char == EMPTY:
-                empties += 1
-    return empties
+    if split == 1:
+        for number, row in enumerate(list):
+            disp = f"{number:02d}: "
+            for char in row:
+                disp += str(char)
+            print(disp)
+    else:
+        for number, row in enumerate(list):
+            disp = f"{number:02d}: "
+            if number % split == 0:
+                for pos, char in enumerate(row):
+                    if pos % split == 0:
+                        disp += str(char)
+                print(disp)
 
 
 def find_start():
@@ -73,22 +92,25 @@ def next_step():
         if "L" in choices and data_list[start[0]][start[1]-1] in ["F", "-", "L"]:
             active_paths[pos] = (start[0], start[1]-1)
         data_list[start[0]][start[1]] = "S"
+        visited.append(active_paths[pos])
     path_length += 1
 
 
 if __name__ == '__main__':
     if DEBUG:
-        file = "data/example2.txt"
+        file = "data/example5.txt"
     else:
         file = "data/puzzle.txt"
     with open(file, "r") as f:
         data_string = f.read()
-    data_list = data_string.split("\n")
-    data_list = ["."*len(data_list[0])]+data_list+["."*len(data_list[0])]
-    data_list = ["." + row + "." for row in data_list]
+    data_string = data_string.split("\n")
+    data_list = [EMPTY*len(data_string[0])]+data_string+[EMPTY*len(data_string[0])]
+    data_list = [EMPTY + row + EMPTY for row in data_list]
     data_list = [list(row) for row in data_list]
-    print(data_list)
+    # pprint(data_list)
+    # pprint(corners_list)
     start = find_start()
+    visited.append(start)
     start_shapes = []
     if data_list[start[0]+1][start[1]] in ["L", "|", "J"]:
         active_paths.append((start[0]+1, start[1]))
@@ -102,18 +124,24 @@ if __name__ == '__main__':
     if data_list[start[0]-1][start[1]] in ["7", "|", "F"]:
         active_paths.append((start[0]-1, start[1]))
         start_shapes.append("U")
+    visited += active_paths
     path_length += 1
-    print(active_paths, path_length)
-    print(start_shapes)
-    start_shape = find_start_shape(start_shapes)
-    # while active_paths[0] != active_paths[1]:
-    #     next_step()
-    #     print(active_paths, path_length)
-    #     # time.sleep(.5)
     # print(active_paths, path_length)
+    # print(start_shapes)
+    start_shape = find_start_shape(start_shapes)
+    while active_paths[0] != active_paths[1]:
+        next_step()
+        # print(active_paths, path_length)
+        # time.sleep(.5)
+    end = active_paths.pop()
+    print(active_paths, path_length)
+    print(visited)
+    # pprint(data_list)
+    data_list = [EMPTY*len(data_string[0])]+data_string+[EMPTY*len(data_string[0])]
+    data_list = [EMPTY + row + EMPTY for row in data_list]
+    data_list = [list(row) for row in data_list]
     data_list[start[0]][start[1]] = start_shape
-    fill_outer_cells(0,0)
-    pprint(data_list)
-    print("\nANSWER = ",count_empties())
+    answer_list, answer = junk_pipe_scan(data_list)
+    print(answer)
 
 # Press the green button in the gutter to run the script.
