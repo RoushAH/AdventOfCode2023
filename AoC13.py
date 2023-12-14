5644dccf760d0f5d3d98b1ee38e6540b2eee803c
RoushAH <Roush.Adam.H@gmail.com>
2023-12-14 06:40:14 +0000

    Day 13 Silver, working to Gold?

--- AoC13.py
+++ AoC13.py
@@ -4,7 +4,7 @@ from multiprocessing.dummy import Pool as ThreadPool
 import functools as ft
 import difflib
 
-DEBUG = True
+DEBUG = False
 HASH = "#"
 
 
@@ -23,36 +23,62 @@ def flipperooni(grid):
             grid[y][x] for y in range(len(grid))
         ]) for x in range(len(grid[0]))
     ]
-    # print("\n".join(grid))
-    # print("\n\n")
-    # print("\n".join(flip))
     return flip
+
+
+def string_hash_job(string,pos):
+    return string[:pos] + HASH + string[pos+1:]
+
+
+def make_and_test_options(grid, options):
+    hits = [[],[]]
+    for option in options:
+        y = option[0]
+        this_grid = grid.copy()
+        this_grid[y] = this_grid[option[1]]
+        print(option)
+        pprint(this_grid)
+        if option[0] == 0:
+            hits[0].append(option[1])
+        elif option[0] == len(this_grid) - 1:
+            hits[1].append(option[1])
+        elif option[1] == 0:
+            hits[0].append(option[0])
+        elif option[1] == len(this_grid) - 1:
+            hits[1].append(option[0])
+        else:
+            hits = find_matches(this_grid, hits)
+        # print(hits)
+        val = test_matches(this_grid, hits, False)
+        if val is not None:
+            return val, hits
+    return None, hits
 
 
 def find_smudge_stage2(grid, possibles):
     options = []
-    for a in possibles:
+    for option in possibles:
+        a, b = option
         row_a = grid[a]
         # print (a, row_a)
-        for b, row_b in enumerate(grid):
-            diff_count = 0
-            diff_locale = 0
-            diff = difflib.ndiff(row_a, row_b)
-            pos = 0
-            for k, s in enumerate(diff):
-                # print(s[0], row_a[k], row_b[k])
-                if s[0] == '-':
-                    diff_count += 1
-                    diff_locale = pos
-                if diff_count == 2:
-                    break
-                pos += 1
-            if diff_count == 1:
-                # print(row_a, row_b, diff_locale)
-                options.append((a, b, diff_locale))
-            # print(a, b, row_a, row_b, diff_count)
+        row_b = grid[b]
+        diff_count = 0
+        diff_locale = 0
+        diff = difflib.ndiff(row_a, row_b)
+        pos = 0
+        for k, s in enumerate(diff):
+            # print(s[0], row_a[k], row_b[k])
+            if s[0] == '-':
+                diff_count += 1
+                diff_locale = pos
+            if diff_count == 2:
+                break
+            pos += 1
+        if diff_count == 1 and (a + b) % 2 == 1 :
+            # print(row_a, row_b, diff_locale)
+            options.append((a, b, diff_locale))
+        # print(a, b, row_a, row_b, diff_count)
     return options
-    pass
 
 
 def find_smudge_stage1(grid):
@@ -62,9 +88,9 @@ def find_smudge_stage1(grid):
     # print(counts)
     possible_rows = set()
     for x, count_a in enumerate(counts):
-        for count_b in counts:
+        for y, count_b in enumerate(counts):
             if count_a == count_b - 1:
-                possible_rows.add(x)
+                possible_rows.add((x,y))
     # print(possible_rows)
     # for a in grid:
     #     for b in grid:
@@ -73,10 +99,10 @@ def find_smudge_stage1(grid):
     return possible_rows
 
 
-def find_matches(grid):
+def find_matches(grid, hits=None):
     candidates_coord = [0, len(grid)-1]
     candidates_row = [grid[0], grid[-1]]
-    hits = [[],[]]
+    hits = hits or [[],[]]
     for i in range(2):
         candidate = candidates_row[i]
         for y, row in enumerate(grid):
@@ -85,7 +111,7 @@ def find_matches(grid):
     return hits
 
 
-def test_matches(grid, hits, debug = False):
+def test_matches(local_grid, hits, debug = False):
     front_hits = hits[0]
     back_hits = hits[1]
     flag = False
@@ -95,31 +121,31 @@ def test_matches(grid, hits, debug = False):
         mirror_width = (hit+1) // 2
         if debug:
             print(hit, mirror_width )
-            # print("\n".join(grid))
-            pprint(grid)
+            # print([(i, local_grid[i] == local_grid[hit - i], local_grid[i], local_grid[hit - i]) for i in range(mirror_width + 1)])
+            pprint(local_grid)
         if all(
-                [grid[i] == grid[hit-i] for i in range(mirror_width+1)]
-                # if exactly 1 False, this is the winner of part 2...
+                [local_grid[i] == local_grid[hit - i] for i in range(mirror_width + 1)]
         ):
             return mirror_width
+        elif debug:
+            print("No match,", [(i, local_grid[i] == local_grid[hit - i], local_grid[i], local_grid[hit - i]) for i in range(mirror_width + 1)])
     for hit in back_hits:
         flag = True
         # print(hit, hit+len(grid)//2, len(grid))
-        mirror_width = (len(grid) - hit)//2
+        mirror_width = (len(local_grid) - hit) // 2
         if debug:
             print("\n\n",hit, mirror_width )
             # print("\n".join(grid))
-            pprint(grid)
+            pprint(local_grid)
         if all(
-            [grid[hit + i] == grid[-(i+1)] for i in range(mirror_width)]
+            [local_grid[hit + i] == local_grid[-(i + 1)] for i in range(mirror_width)]
         ):
             # print("Winner!",hit+len(grid)//2)
             # winner
             return hit+mirror_width
     if flag and debug:
-        print("\n".join(grid), "\n", hits)
-        print("\n\n".join(debug_data))
-        input()
+        print("\n".join(local_grid), "\n", hits)
+        # print("\n\n".join(debug_data))
     pass
 
 
@@ -136,6 +162,7 @@ if __name__ == '__main__':
     # print(test_matches(data_list[1], find_matches(data_list[1])))
     # print(find_matches(data_list[1]))
     answer = 0
+    answer2 = 0
     for grid in data_list:
         grid_val = test_matches(grid, find_matches(grid))
         if grid_val is not None:
@@ -144,7 +171,22 @@ if __name__ == '__main__':
             f_grid = flipperooni(grid)
             answer += test_matches(f_grid, find_matches(f_grid), False) or 0
             # input()
-        possibles = find_smudge_stage1(data_list[1])
-        print(find_smudge_stage2(grid, possibles))
+        possibles = find_smudge_stage1(grid)
+        options = find_smudge_stage2(grid, possibles)
+        grid_val_2, hits = make_and_test_options(grid, options)
+        if grid_val_2 is not None:
+            answer2 += 100 * grid_val_2
+        else:
+            possibles = find_smudge_stage1(f_grid)
+            options = find_smudge_stage2(f_grid, possibles)
+            grid_val_2, f_hits = make_and_test_options(f_grid, options)
+            print(grid_val_2, f_hits, hits)
+            pprint(f_grid)
+            pprint(grid)
+            answer2 += grid_val_2
+
     print(answer)
+    print("Second:",answer2)
+
+    # Need to consider idea that smudge is in a throwaway row/column
 
